77de74e0b5d911074d634b8ba3e0fed7e0da14ee
pablodanswer <pablo@danswer.ai>
2024-12-11 16:46:10 -0800

    k

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -77,4 +77,7 @@ export const REGISTRATION_URL =
 export const SERVER_SIDE_ONLY__CLOUD_ENABLED =
   process.env.NEXT_PUBLIC_CLOUD_ENABLED?.toLowerCase() === "true";
 
+export const NEXT_PUBLIC_FORGOT_PASSWORD_ENABLED =
+  process.env.NEXT_PUBLIC_FORGOT_PASSWORD_ENABLED?.toLowerCase() === "true";
+
 export const TEST_ENV = process.env.TEST_ENV?.toLowerCase() === "true";
